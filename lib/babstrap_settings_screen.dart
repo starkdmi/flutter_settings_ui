47d574f67954c7c3927971240d7d4a9e946aa780
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2022-04-18 20:34:40 +0300

    Remove optional files

--- lib/babstrap_settings_screen.dart
+++ lib/babstrap_settings_screen.dart
@@ -8,6 +8,3 @@ export 'src/settings_screen_utils.dart';
 export 'src/icon_style.dart';
 export 'src/babs_component_settings_item.dart';
 export 'src/babs_component_settings_group.dart';
-export 'src/babs_component_big_user_card.dart';
-export 'src/babs_component_small_user_card.dart';
-export 'src/babs_component_simple_user_card.dart';
